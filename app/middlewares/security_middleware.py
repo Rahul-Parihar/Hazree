abff0e0d56ffd2bac0e78e14b24d75c379975ea1
Anandpatel54 <panand054@gmail.com>
2026-08-14 11:33:22 +0530

    Completed

--- app/middlewares/security_middleware.py
+++ app/middlewares/security_middleware.py
@@ -12,7 +12,6 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
 
-        # Injected security headers
         response.headers["X-Content-Type-Options"] = "nosniff"
         response.headers["X-Frame-Options"] = "DENY"
         response.headers["X-XSS-Protection"] = "1; mode=block"
